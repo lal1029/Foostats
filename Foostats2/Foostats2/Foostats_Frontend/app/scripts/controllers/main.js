2feb4c4966a90e27a9f4614cc4850cb528d8ca7b
lizerspockzebrafish <llsaphire1029@gmail.com>
2014-04-27 17:36:24 -0700

    Modifying Foostat front end using angular

---
+++ Foostats2/Foostats2/Foostats_Frontend/app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('foostatsApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
